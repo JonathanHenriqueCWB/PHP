00914ee366d0047eef28bb482bf7650789d987df
Jonathan <jonathanhenriquecwb@gmail.com>
2018-06-28 17:19:45 -0300

    Estruturas de Repetição

---
+++ Aula007_EstruturasDeRepeticao/RepeticaoWhile/exercicio001.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <?php
+
+        $x=0;
+        $y=10;
+
+        /* Executara enquanto x for menor que 10 */
+        while ($x < 10) {
+            echo $x ++;
+        }
+
+        echo "<br>";
+
+        /*Executara enquanto y for maior que 0*/
+        while ($y > 0) {
+            echo  $y --;
+        }    
+
+    ?>
+</body>
+</html>
